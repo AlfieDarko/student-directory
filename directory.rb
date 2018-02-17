62158c2e53b9564b5f726f85c51add801c059b99
Alfie Darko <mrhazzy@gmail.com>
2018-02-17 19:49:51 +0000

    added load from csv feature

--- directory.rb
+++ directory.rb
@@ -117,6 +117,7 @@ def print_menu
   puts '1. Input the students'
   puts '2. Show the students'
   puts '3. Save the list to students.csv'
+  puts '4. Load the list from students.csv'
   puts '9. Exit' # 9 because we'll be adding more items
 end
 
@@ -149,6 +150,15 @@ def save_students
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(',')
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  fie = File.open('students.csv', 'r')
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @student << { name: name, cohort: cohort.to_sym }
   end
   file.close
 end
